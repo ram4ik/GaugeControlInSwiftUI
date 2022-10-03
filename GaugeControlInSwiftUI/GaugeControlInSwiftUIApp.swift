64efd315c30deac19ab5fe1038e7097a9aee2b80
Ramill <ramill_ibragimov@icloud.com>
2022-10-03 07:57:27 +0300

    Initial Commit

---
+++ GaugeControlInSwiftUI/GaugeControlInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeControlInSwiftUIApp.swift
+//  GaugeControlInSwiftUI
+//
+//  Created by Ramill Ibragimov on 03.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct GaugeControlInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
